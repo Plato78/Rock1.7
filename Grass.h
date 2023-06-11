e31fecd153b0b260df08c626503e17544f42bdf0
Plato78 <114007402+Plato78@users.noreply.github.com>
2023-06-11 20:45:49 +0300

    Update Grass.h

--- Grass.h
+++ Grass.h
@@ -17,24 +17,3 @@ public:
 		window.draw(sprite);
 	}
 };
-
-class startScreen {
-private:
-	sf::Texture texture;
-	sf::Sprite sprite;
-public:
-	startScreen(sf::Vector2f pos, std::string fileName) {
-		texture.loadFromFile(fileName);
-		sprite.setTexture(texture);
-		sprite.setPosition(pos);
-	}
-	void removeImageOnSpace(int score = 0) {
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-			texture.loadFromFile("");
-			sprite.setTexture(texture);
-		}
-	}
-	void draw(sf::RenderWindow& window) {
-		window.draw(sprite);
-	}
-};
